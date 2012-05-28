40561b319fd9b7d3b7e730cc36610ec7629a1e10
EXio 4 <exio4.com@gmail.com>
2012-05-28 11:23:36 -0300

    added debugs functions to mini-tun device

--- libs/net/tun.c
+++ libs/net/tun.c
@@ -4,16 +4,20 @@
 #include <net/if.h>
 #include <linux/if_tun.h>
 #include <sys/ioctl.h>
+#include <libs/debug/debug.h>
 
 int TUN_ALLOCATE(char *name, char *interfacename,int mode) {
 	struct ifreq ifr;
 	int fd;
-	if ( (fd = open("/dev/net/tun",O_RDWR)) < 0) return -1;
+
+	DEBUG("opening tun device .... \n");
+	if ( (fd = open("/dev/net/tun",O_RDWR)) < 0) { DEBUG("[%s] error in [open]\n",name); return -1; }
 	memset(&ifr, 0, sizeof(ifr));
 	ifr.ifr_flags = mode;
 	strncpy(ifr.ifr_name, name, IFNAMSIZ);
-	if (ioctl(fd, TUNSETIFF, (void *)&ifr) < 0) return -2;
-
+	DEBUG("ok! allocating tun device\n");
+	if (ioctl(fd, TUNSETIFF, (void *)&ifr) < 0) { DEBUG("[%s] error in [ioctl]\n",name); return -2; }
+	DEBUG("tun device allocated..n");
 	interfacename=malloc(strlen(ifr.ifr_name+1));
 	strcpy(interfacename,ifr.ifr_name);
 	return fd;
