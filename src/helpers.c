ba65733cd6f8c54cbbb56b5fe9d1ac2208a0318f
Esteban I. Ruiz Moreno <exio4.com@gmail.com>
2013-08-12 17:25:05 -0300

    fix idents and add more b64 tests

--- src/helpers.c
+++ src/helpers.c
@@ -2,13 +2,14 @@
 #include <unistd.h>
 
 int split(char *str, char **fstr) {
-     char *rest;
-     char *p;
-     int i=0;
+    char *rest;
+    char *p;
+    int i=0;
 
-     for(p = strtok_r(str,"\n", &rest); p; p = strtok_r(NULL, "\n", &rest)) {
-               fstr[i] = p;
-               i++;
-     }
-     return i;
+    for(p = strtok_r(str,"\n", &rest); p; p = strtok_r(NULL, "\n", &rest)) {
+        fstr[i] = p;
+        i++;
+    }
+
+    return i;
 }
