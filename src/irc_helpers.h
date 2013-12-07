c96732f5829d08fc3231b2cd33a88f59dcd39de1
Esteban I. Ruiz Moreno <exio4.com@gmail.com>
2013-12-07 16:37:15 -0300

    Revert flood control
    (Commits 20f3ce0818d61c18aa1ddf2dddd286fbe685b7b9 and 272372845943b7398c8bed9c751bfce86a482b5b)

--- src/irc_helpers.h
+++ src/irc_helpers.h
@@ -1,7 +1,4 @@
 #ifndef _IRC_HELPERS_H
 #define _IRC_HELPERS_H
 void irc_debug(irc_thread_data *self, char * format, ...);
-int64_t tms_diff (struct timespec *timeA_p, struct timespec *timeB_p);
-int lsleep(float time);
-int ms_delay(int ms);
 #endif
